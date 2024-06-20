1442746eaa6c4bfeec4ea3e13a621889cd8b8432
Thales Gelinger <thalesgelinger@gmail.com>
2024-06-20 12:28:02 -0300

    feat: add toggle backgorund color

--- .config/nvim/after/plugin/color.lua
+++ .config/nvim/after/plugin/color.lua
@@ -1,4 +1,4 @@
-function LineNumberColors()
+function defaultTransparent()
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
     vim.cmd.colorscheme "tokyonight"
@@ -8,4 +8,19 @@ function LineNumberColors()
     vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#dedede', bold = false })
 end
 
-LineNumberColors()
+local is_transparent = true
+
+-- Toggle Transparent Background
+vim.keymap.set("n", "<leader>bb", function()
+    if is_transparent then
+        vim.cmd.colorscheme "tokyonight"
+        vim.api.nvim_set_hl(0, "Normal", { bg = "#24283b" })
+        vim.api.nvim_set_hl(0, "NormalFloat", { bg = "#24283b" })
+        is_transparent = false
+    else
+        defaultTransparent()
+        is_transparent = true
+    end
+end)
+
+defaultTransparent()
